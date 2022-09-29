931b756102f5813f5431604781a1121a53a7c0a2
Moisés Fernández Muiña <mochyfm@gmail.com>
2022-09-29 12:31:13 +0100

    Feature: Modified App.js
    Modified the App.js file, and created a folder in assets. Its planned
    to modify this file and create it with components.

--- App.js
+++ App.js
@@ -1,13 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View>
-      <Text>Cleared</Text>
+    <View style={styles.container}>
+      <View style={styles.nameBanner}>
+        <Image style={styles.prof_Picture} source={require('./assets/profile/example.jpg')}></Image>
+        <Text style={styles.nameText}>Moisés Fernández Muiña</Text>
+      </View>
+      <View style={styles.biography}>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  nameBanner: {
+    backgroundColor: '#808080',
+    height: 100,
+    paddingTop: 30,
+    paddingRight: 20,
+    paddingBottom: 10,
+    paddingLeft: 20,
+    
+
+    flex: 1,
+    flexDirection: 'row',
+
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  prof_Picture: {
+    borderRadius: 50,
+    flex: 2,
+    width: 90,
+    height: 80
+  },
+  nameText: {
+    flex: 7,
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+    fontSize: 22,
+    paddingLeft: 10,
+    textAlign: 'left'
+  },
+  biography: {
+    flex: 5
+  }
+
 });
