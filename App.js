e974b7d807769c1dfb5ba55a695b77b544ea3e3a
Moisés Fernández Muiña <mochyfm@gmail.com>
2022-09-30 08:23:02 +0100

    Feature: Created component for NameBanner
    Changed the method of creation of the header with the username and the
    profile picture. Now its made with a component, that is inside of
    the "Components" folder where we will store all created componentes
    (each one with its own folder) and the content of its own.

--- App.js
+++ App.js
@@ -1,54 +1,20 @@
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import NameBanner from './Components/NameBanner';
 
-export default function App() {
+const App = () => {
+
   return (
     <View style={styles.container}>
-      <View style={styles.nameBanner}>
-        <Image style={styles.prof_Picture} source={require('./assets/profile/example.jpg')}></Image>
-        <Text style={styles.nameText}>Moisés Fernández Muiña</Text>
-      </View>
-      <View style={styles.biography}>
-
-      </View>
+      <NameBanner style={styles.nameBanner} givenName={'Moisés Fernández Muiña'}/>
     </View>
   );
+
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  nameBanner: {
-    backgroundColor: '#808080',
-    height: 100,
-    paddingTop: 30,
-    paddingRight: 20,
-    paddingBottom: 10,
-    paddingLeft: 20,
-    
-
-    flex: 1,
-    flexDirection: 'row',
-
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  prof_Picture: {
-    borderRadius: 50,
-    flex: 2,
-    width: 90,
-    height: 80
-  },
-  nameText: {
-    flex: 7,
-    color: '#FFFFFF',
-    fontWeight: 'bold',
-    fontSize: 22,
-    paddingLeft: 10,
-    textAlign: 'left'
-  },
-  biography: {
-    flex: 5
-  }
-
 });
